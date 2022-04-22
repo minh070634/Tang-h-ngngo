e20fed153f828d8a6aa24a8f14847c2d2c4a7558
minh070634 <87203590+minh070634@users.noreply.github.com>
2022-04-23 03:09:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,19 @@
 const CONFIG = {
-    introTitle: 'Hế lu Hằng Ngô !',
-    introDesc: 'Anh có điều này muốn nói với em nè ',
+    introTitle: 'hế lu Hằng Ngô',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: ' Đã từ rất lâu rồi a muốn hỏi e là : e iu a nhìu lắm chứ  ',
-    desc: 'iu a nhiều không nè ',
-    btnYes: 'có nè ',
-    btnNo: ' không nè  ',
-    question:'Trên thế giới hơn 7 tỉ người mà sao iu e nhìu thế nhỉ <3 ',
-    btnReply: ' Gửi cho a câu trả lời nhé <3',
-    reply: 'Tại vì a đẹp trai lắm ạ   ',
-    mess: ' vậy thì nao đi chơi với a nhé ',
-    messDesc: ' hẹn e 7h tối ngày 27 nha ',
-    btnAccept: 'Okiii nè <3',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙 A có điều muốn nói ạ ',
+    desc: ' hí hí e có yêu a nhiều không vậy ',
+    btnYes: 'Vẫn cứ là iu anh <33',
+    btnNo: 'Không nha a xấu trai lại còn da đen  :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao e lại yêu a nhiều thế nhỉ <3',
+    btnReply: 'Gửi cho anh nhá <3',
+    reply: 'Tại vì a đẹp trai <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều nắm 😘😘',
+    messDesc: 'Hẹn e tối 27 đi chơi vs a nhá , anh qua đón e nhé.',
+    btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
