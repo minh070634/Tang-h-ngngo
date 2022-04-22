51bfdd257890398dfeda147401d1883fb42dd9a5
minh070634 <87203590+minh070634@users.noreply.github.com>
2022-04-23 02:41:06 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,15 +2,15 @@ const CONFIG = {
     introTitle: 'Hế lu Hằng Ngô !',
     introDesc: 'Anh có điều này muốn nói với em nè ',
     btnIntro: 'hihi',
-    title: ' Đã từ rất lâu rồi a muốn hỏi e là : e iu a nhìu chứ  ',
+    title: ' Đã từ rất lâu rồi a muốn hỏi e là : e iu a nhìu lắm chứ  ',
     desc: 'iu a nhiều không nè ',
     btnYes: 'có nè ',
     btnNo: ' không nè  ',
     question:'Trên thế giới hơn 7 tỉ người mà sao iu e nhìu thế nhỉ  <3 ',
     btnReply: ' Gửi cho a câu trả lời nhé <3',
-    reply: 'Tại vì a đẹp trai đúng không  ',
+    reply: 'Tại vì a đẹp trai lắm ạ   ',
     mess: ' vậy thì nao đi chơi với a nhé ',
     messDesc: ' hẹn e 7h tối ngày 27 nha ',
-    btnAccept: 'Okiii <3',
+    btnAccept: 'Okiii nè <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
